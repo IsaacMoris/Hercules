0a712e8bf36c4a38f38d14cbceb4c80826d5223d
Youssef Nagy <44213230+josefnagy1999@users.noreply.github.com>
2020-03-28 23:40:14 +0200

    Physics 2

--- src/mygame/level1_scene.java
+++ src/mygame/level1_scene.java
@@ -4,40 +4,31 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
-import com.jme3.input.controls.Trigger;
+import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.control.CameraControl;
 
-public class level1_scene extends SimpleApplication {
+public class level1_scene extends SimpleApplication implements ActionListener {
 
     Node Scene;
     Node player;
     private BulletAppState bulletAppState;
     private RigidBodyControl scenePhy;
     private BetterCharacterControl playerControl;
-    // Controls
-    private final static Trigger Jump = new KeyTrigger(KeyInput.KEY_SPACE);
-    private final static Trigger Move_Forward = new KeyTrigger(KeyInput.KEY_UP);
-    private final static Trigger Move_Backward = new KeyTrigger(KeyInput.KEY_DOWN);
-    private final static Trigger Move_Right = new KeyTrigger(KeyInput.KEY_RIGHT);
-    private final static Trigger Move_Left = new KeyTrigger(KeyInput.KEY_LEFT);
-
-    private final static String JumpS = "Jump";
-    private final static String Move_ForwardS = "Move Forward";
-    private final static String Move_BackwardS = "Move Backward";
-    private final static String Move_RightS = "Move Right";
-    private final static String Move_LeftS = "Move Left";
-
+    Spatial floor;
     @Override
     public void simpleInitApp() {
+
+       
+        
         bulletAppState = new BulletAppState();  //Physics Lib
         stateManager.attach(bulletAppState);
 
@@ -48,92 +39,104 @@ public class level1_scene extends SimpleApplication {
         Scene.setLocalTranslation(0, 0, 0);
         bulletAppState.getPhysicsSpace().add(Scene);
         bulletAppState.getPhysicsSpace().setGravity(new Vector3f(0, -9.81f, 0));
-        //  bulletAppState.getPhysicsSpace().setAccuracy(0.016f);
+        bulletAppState.getPhysicsSpace().setAccuracy(0.016f);
         rootNode.attachChild(Scene);
 
         player = (Node) Scene.getChild("Player"); // Player Attachment
+        player.center();
         // Player Physics
-        player.setLocalTranslation(new Vector3f(0,10f ,0 ));
 
-        playerControl = new BetterCharacterControl(3f, 7f, 30f);    //        playerControl = new BetterCharacterControl(1.7f, 2.2f, 30f);
-        playerControl.setJumpForce(new Vector3f(0, 500f, 0));
-        playerControl.setGravity(new Vector3f(0, -10f, 0));
-        playerControl.warp(new Vector3f(0, 10, 10));
+        player.setLocalTranslation(new Vector3f(0, 10f, 0));
+        playerControl = new BetterCharacterControl(1f, 1f, 10f);    //        playerControl = new BetterCharacterControl(1.7f, 2.2f, 30f);
+        playerControl.setJumpForce(new Vector3f(0, 50f, 0));
+        playerControl.setGravity(new Vector3f(0, 10f, 0));
+        playerControl.warp(new Vector3f(0, 10, 0));
+        bulletAppState.getPhysicsSpace().add(playerControl);
 
         player.addControl(playerControl);
 
-        bulletAppState.getPhysicsSpace().add(playerControl);
-
-        flyCam.setEnabled(false);
-        flyCam.setMoveSpeed(200);
-        flyCam.setZoomSpeed(200);
-        ChaseCamera chaseCam = new ChaseCamera(cam, player, inputManager);
-        chaseCam.setSmoothMotion(true);
+        bulletAppState.setDebugEnabled(true);
+//        ChaseCamera chaseCam = new ChaseCamera(cam, player, inputManager);
+//        chaseCam.setSmoothMotion(true);
         // Controls Mapping
-        inputManager.addMapping(JumpS, Jump);
-        inputManager.addMapping(Move_ForwardS, Move_Forward);
-        inputManager.addMapping(Move_BackwardS, Move_Backward);
-        inputManager.addMapping(Move_LeftS, Move_Left);
-        inputManager.addMapping(Move_RightS, Move_Right);
-        //Controls Listners
-        inputManager.addListener(actionListener, new String[]{JumpS});
-        inputManager.addListener(actionListener, new String[]{Move_ForwardS});
-        inputManager.addListener(actionListener, new String[]{Move_LeftS});
-        inputManager.addListener(actionListener, new String[]{Move_RightS});
-        inputManager.addListener(actionListener, new String[]{Move_BackwardS});
-
+        inputManager.addMapping("Forward",
+                new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Back",
+                new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Rotate Left",
+                new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Rotate Right",
+                new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Jump",
+                new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Rotate Left", "Rotate Right");
+        inputManager.addListener(this, "Forward", "Back", "Jump");
     }
 
+    private Vector3f walkDirection = new Vector3f(0, 0, 0);
+    private Vector3f viewDirection = new Vector3f(0, 0, 1);
+    private boolean rotateLeft = false, rotateRight = false,
+            forward = false, backward = false;
+    private float speed = 8;
     //Action Listners
-    private ActionListener actionListener = new ActionListener() {
-        @Override
-        public void onAction(String name, boolean isPressed, float tpf) {
-            System.out.println("You triggered: " + name);
-            if (!isPressed) {
-                playerControl.setWalkDirection(new Vector3f(0, 0, 0));
-            }
-            if (name.equals(JumpS) && !isPressed) {
-                // implement action here
-                // player.move(0, 1, 0);
-                playerControl.jump();
 
-            }
-            if (name.equals(Move_ForwardS) && isPressed) {
-                // implement action here
-
-                //  player.move(-1, 0, 0);
-                playerControl.setWalkDirection(new Vector3f(-10, 0, 0));
-
-            }
-
-            if (name.equals(Move_BackwardS) && isPressed) {
-                // implement action here
-
-                player.move(1, 0, 0);
-                playerControl.setWalkDirection(new Vector3f(10, 0, 0));
-
-            }
-            if (name.equals(Move_LeftS) && isPressed) {
-                // implement action here
-                //  player.move(0, 0, 1);
-                playerControl.setWalkDirection(new Vector3f(0, 0, 10));
-
-            }
-            if (name.equals(Move_RightS) && isPressed) {
-                // implement action here
-                player.move(0, 0, -1);
-                playerControl.setWalkDirection(new Vector3f(0, 0, -10));
-
-            }
+    @Override
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Rotate Left")) {
+            rotateLeft = isPressed;
+        } else if (binding.equals("Rotate Right")) {
+            rotateRight = isPressed;
+        } else if (binding.equals("Forward")) {
+            forward = isPressed;
+        } else if (binding.equals("Back")) {
+            backward = isPressed;
+        } else if (binding.equals("Jump")) {
+            playerControl.jump();
         }
-    };
+    }
+
+    private CameraNode camNode;
 
     @Override
     public void simpleUpdate(float tpf) {
 
-        // player.move(-0.02f, 0.01f, 0.01f);
-        System.out.println(player.getLocalTranslation());
+        // System.out.println(player.getLocalTranslation());
+        // Disable the default flyby cam
+        flyCam.setEnabled(false);
+        //create the camera Node
+        camNode = new CameraNode("Camera Node", cam);
+        //This mode means that camera copies the movements of the target:
+        camNode.setControlDir(CameraControl.ControlDirection.SpatialToCamera);
+        //Attach the camNode to the target:
+        player.attachChild(camNode);
+        //Move camNode, e.g. behind and above the target:
+        camNode.setLocalTranslation(new Vector3f(-5, 5, -9));
+        //Rotate the camNode to look at the target:
+        camNode.lookAt(player.getLocalTranslation(), Vector3f.UNIT_Y);
 
+        // Get current forward and left vectors of the playerNode:
+        Vector3f modelForwardDir = player.getWorldRotation().mult(Vector3f.UNIT_Z);
+        Vector3f modelLeftDir = player.getWorldRotation().mult(Vector3f.UNIT_X);
+        // Determine the change in direction
+        walkDirection.set(0, 0, 0);
+        if (forward) {
+            walkDirection.addLocal(modelForwardDir.mult(speed));
+        } else if (backward) {
+            walkDirection.addLocal(modelForwardDir.mult(speed).
+                    negate());
+        }
+        playerControl.setWalkDirection(walkDirection); // walk!
+        // Determine the change in rotation
+        if (rotateLeft) {
+            Quaternion rotateL = new Quaternion().
+                    fromAngleAxis(FastMath.PI * tpf, Vector3f.UNIT_Y);
+            rotateL.multLocal(viewDirection);
+        } else if (rotateRight) {
+            Quaternion rotateR = new Quaternion().
+                    fromAngleAxis(-FastMath.PI * tpf, Vector3f.UNIT_Y);
+            rotateR.multLocal(viewDirection);
+        }
+        playerControl.setViewDirection(viewDirection); // turn!
     }
 
     @Override
